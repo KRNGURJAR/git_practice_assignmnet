cc0437a52d0dcd744358c1d4a5c6b744cac8e632
KARAN GAIRATHI <karangarathi2022@gmail.com>
2022-11-29 23:20:04 +0530

    check code

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let str="civic";
+let n=str.length
+for(i=5-1;i>=0;i--){
+    bag=str[i]	
+}
+if (bag==str){
+	console.log(true)
+}
+else if {
+console.log(false)
+}
---
+++ prime.js
@@ -0,0 +1,13 @@
+ let num=5;
+let count=0;
+for(i=1;i<5;i+){
+	if(num%1!=0){
+count++;
+}	
+}
+if(count==2){
+console.log(prime)
+}
+else{
+console.log(false)
+}
